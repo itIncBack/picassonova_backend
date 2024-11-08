cad71eeba0f3607f9f3e45592863bb92f8673178
nikita <nikita.lavrentyev@mysky.com>
2024-11-08 11:10:32 +0400

    test

--- apps/files/src/main.ts
+++ apps/files/src/main.ts
@@ -3,10 +3,12 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { GatewayModule } from '../../gateway/src/gateway.module';
 import * as process from 'process';
 import { config } from 'dotenv';
+import { FilesModule } from '@apps/files/src/files.module';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    GatewayModule,
+    // GatewayModule,
+    FilesModule,
     {
       transport: Transport.TCP,
       options: {
