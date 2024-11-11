9a5c75f80d6a3ea74bcf787333f9ebd574a162f2
nikita <nikita.lavrentyev@mysky.com>
2024-11-11 11:29:04 +0400

    test

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -13,7 +13,6 @@ async function bootstrap() {
     app.get(ConfigService);
   const apiSettings = configService.get('apiSettings', { infer: true });
 
-  console.log("sadaasdasd");
   await app.listen(apiSettings.PORT, () => {
     console.log('Gateway App starting listen port: ', apiSettings.PORT);
   });
