cc010fc07f7a2ee9ada85d320aed1b5efbe7ef94
nikita <nikita.lavrentyev@mysky.com>
2024-11-11 11:38:33 +0400

    test

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -9,6 +9,8 @@ async function bootstrap() {
 
   applyAppSettings(app);
 
+  console.log('test');
+
   const configService: ConfigService<Configuration, true> =
     app.get(ConfigService);
   const apiSettings = configService.get('apiSettings', { infer: true });
