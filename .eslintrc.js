d2e37243a511a9ae1f9125993afa46aaa8e29ade
nikita <nikita.lavrentyev@mysky.com>
2024-11-22 15:13:26 +0400

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,9 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-extra-semi': 'error', // для дополнительного контроля
     '@typescript-eslint/no-unsafe-member-access': 'warn',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-extra-semi': 'off'
   },
 };
--- apps/files/test/app.e2e-spec.ts
+++ apps/files/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { FilesModule } from '../src/files.module';
 
 describe('FilesController (e2e)', () => {
