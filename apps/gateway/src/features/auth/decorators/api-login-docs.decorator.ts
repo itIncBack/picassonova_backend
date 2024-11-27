062b8c013a2d7a598c33fe69fe37cde33ca89ce5
skitchen1992 <67601738+skitchen1992@users.noreply.github.com>
2024-11-27 10:06:47 +0400

    feat: add log (#6)
    Co-authored-by: nikita <nikita.lavrentyev@mysky.com>

---
+++ apps/gateway/src/features/auth/decorators/api-login-docs.decorator.ts
@@ -0,0 +1,28 @@
+import { applyDecorators, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AccessTokenSchema, BadRequestSchema } from './schemas'; // Импортируйте ваши схемы
+
+export function ApiLoginDocs() {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Login',
+      description: 'Try login user to the system',
+    }),
+    ApiResponse({
+      status: HttpStatus.OK,
+      schema: AccessTokenSchema,
+      description:
+        'Returns JWT accessToken (expired after 24h) in body and JWT refreshToken in cookie (http-only, secure) (expired after 48 hours).',
+    }),
+    ApiResponse({
+      status: HttpStatus.BAD_REQUEST,
+      description: 'If the provided data is invalid.',
+      schema: BadRequestSchema,
+    }),
+    ApiResponse({
+      status: HttpStatus.UNAUTHORIZED,
+      description:
+        'If the password or email is incorrect, or the email is not confirmed.',
+    }),
+  );
+}
