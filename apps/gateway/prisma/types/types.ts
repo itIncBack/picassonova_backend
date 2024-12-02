015149816e9af7ffc455699c25ebd23bf2126f76
nikita <nikita.lavrentyev@mysky.com>
2024-12-02 14:50:26 +0400

    fix: db9

--- apps/gateway/prisma/types/types.ts
+++
@@ -1,9 +0,0 @@
-export enum ConfirmationType {
-  EMAIL_VERIFICATION = 'EMAIL_VERIFICATION',
-  PASSWORD_RECOVERY = 'PASSWORD_RECOVERY',
-}
-
-export enum Role {
-  BASIC = 'BASIC',
-  ADMIN = 'ADMIN',
-}
