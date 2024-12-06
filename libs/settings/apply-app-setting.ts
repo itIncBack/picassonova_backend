b5f4b59cdab70fe603a9250170f77be47288e59b
nikita <nikita.lavrentyev@mysky.com>
2024-12-06 15:27:58 +0400

    fix: CookieService 24

--- libs/settings/apply-app-setting.ts
+++ libs/settings/apply-app-setting.ts
@@ -56,14 +56,19 @@ const setEnableCors = (app: INestApplication) => {
     'http://localhost:63342',
   ];
 
+  // app.enableCors({
+  //   origin: (origin, callback) => {
+  //     if (!origin || allowedOrigins.includes(origin)) {
+  //       callback(null, true);
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'));
+  //     }
+  //   },
+  //   credentials: true,
+  // });
+
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: '*', // Разрешить все источники временно для отладки
     credentials: true,
   });
 };
