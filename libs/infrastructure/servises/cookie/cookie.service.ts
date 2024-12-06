a10e703bb409f5d74186bc6c4c0dc74f3f4dbc97
nikita <nikita.lavrentyev@mysky.com>
2024-12-06 12:27:52 +0400

    fix: CookieService 12

--- libs/infrastructure/servises/cookie/cookie.service.ts
+++ libs/infrastructure/servises/cookie/cookie.service.ts
@@ -47,7 +47,7 @@ export class CookieService {
       domain: 'localhost',
       maxAge: 24 * 60 * 60 * 1000, // 1 day
       secure: false,
-      httpOnly: true,
+      //httpOnly: true,
       //domain: 'picassonova.online',
       //path: '/',
       //secure: this.environmentSettings.isProduction(),
